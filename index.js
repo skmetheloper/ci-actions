87b1b27de864a3bbdb31251f4572827275bc4a48
Shin Maung Maung <ethereal97@gmail.com>
2020-05-28 22:52:26 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+// index.js
 const core = require('@actions/core');
 
-let inputs = [
-  core.getInput('user'),
-  core.getInput('token'),
-  core.getInput('message'),
-  core.getInput('content'),
-  core.getInput('branch'),
-  core.getInput('file'),
-  core.getInput('dist')
-];
+const GITHUB = {
+    USER: core.getInput('user'),
+    TOKEN: core.getInput('token'),
+    BRANCH: core.getInput('branch'),
+    REPO: core.getInput('repo'),
+    COMMIT: {
+        message: core.getInput('message'),
+        content: core.getInput('content'),
+        source: core.getInput('file'),
+        destination: core.getInput('dist') || core.getInput('file')
+    },
+    get CREDENTIAL() {
+        return `https://${this.USER}:${this.TOKEN}@api.github.com/contents/${this.REPO}`;
+    },
+    get data() {
+        let result = {
+            message: this.COMMIT.message,
+            branch: this.branch,
+            content: ''
+        };
 
-console.log(inputs);
+        if ('sha' in this.COMMIT) {
+            result.sha = this.COMMIT.sha;
+        }
+
+        result.content = btoa(result.content);
+
+        return result;
+    },
+    get source() {
+        let [path, branch] = this.COMMIT.source.split('@');
+        typeof branch === 'undefined' && (branch = this.BRANCH);
+        return `${this.CREDENTIAL}/${path}?ref=${branch}`;
+    },
+    get destination() {
+        let [file, branch] = this.COMMIT.destination.split('@');
+        typeof branch === 'undefined' && (branch = this.BRANCH);
+        return `${this.CREDENTIAL}/${path}?ref=${branch}`;
+    }
+};
+
+console.log([
+  GITHUB.CREDENTIAL,
+  GITHUB.source, GITHUB.destination, GITHUB.data
+]);
